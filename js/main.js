0f1e3a23cfcca7108cd561add7ae69697b7e389c
Fuller <s_fuller3@tss-hr420-dm15.fc.ca>
2019-01-14 16:22:59 -0500

    adding js and index

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(() => {
+	// self invoking anonymous function or lambda
+	var images = document.querySelectorAll('img');
+	// var => means create a variable
+	// whatever comes after the eqauls sign is what the variable 
+	// 
+	// document is always the webpage
+	// after the dot is always a method or a prperty
+		console.log(images);
+
